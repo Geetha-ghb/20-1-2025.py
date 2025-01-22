2780dad21a63090b58471ff2f490648688ba3100
Geetha-ghb <guggillageethanjali@gmail.com>
2025-01-22 11:27:52 +0530

    day-01

---
+++ day-01.py
@@ -0,0 +1,22 @@
+print('Arithematic operations')
+num1= 21
+num2=24
+print(num1+ num2)
+print(num1- num2)
+#if num 2 value is changed to num2=56 then
+num2=56
+print(num1* num2)
+print(num1/ num2)
+print(num1// num2)
+#strings
+str1= "em antivi em antivi....."
+print(len(str1))
+print(str1[4])
+print(str1[0: 3])
+#To calculate the powers
+num=6
+print(num**2)
+#To print string
+print("Em antivi em antivi...")
+
+
